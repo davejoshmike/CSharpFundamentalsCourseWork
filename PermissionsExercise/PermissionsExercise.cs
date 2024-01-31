d8e5c67fd75898904e04f6e7bcb3265ccb8febfd
David Michel <davejoshmike@gmail.com>
2024-01-31 15:55:16 -0500

    Finished Do While Exercise Two

--- PermissionsExercise/PermissionsExercise.cs
+++ PermissionsExercise/PermissionsExercise.cs
@@ -11,13 +11,17 @@ namespace CSharpFundamentalsCourse.PermissionsExercise
         
             string permission = "Admin|Manager";
             int level = 55;
-            bool hasPermissions = CheckPermissionLevels(level, permission, out string message);
+            bool hasPermissions = CheckPermissionLevelsExerciseOne(level, permission, out string message);
             string hasPermissionsMsg = hasPermissions ? "" : "Access Denied.";
             Console.WriteLine($"{hasPermissionsMsg} {message}");
+            Console.Write($"Press Enter to Continue...");
+            Console.ReadLine();
+
+            CheckPermissionsLevelExerciseTwoDoWhile();
         }
 
         // returns whether a user has the correct permissions and level ratings
-        private static bool CheckPermissionLevels(int level, string permission, out string message)
+        private static bool CheckPermissionLevelsExerciseOne(int level, string permission, out string message)
         {
             bool isAdmin = permission.Contains("Admin");
             bool isManager = permission.Contains("Manager");
@@ -48,6 +52,37 @@ namespace CSharpFundamentalsCourse.PermissionsExercise
                 message = "You do not have sufficient privileges.";
                 return false;
             }
+        }
+
+        private static void CheckPermissionsLevelExerciseTwoDoWhile()
+        {
+            string? readResult;
+            string roleName = "";
+            bool validEntry = false;
+
+            do
+            {
+                Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+                readResult = Console.ReadLine();
+                if (readResult != null)
+                {
+                    roleName = readResult.Trim();
+                }
+
+                if (roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user")
+                {
+                    validEntry = true;
+                }
+                else
+                {
+                    Console.Write($"The role name that you entered, \"{roleName}\" is not valid. ");
+                }
+
+            } while (validEntry == false);
+
+            Console.WriteLine($"Your input value ({roleName}) has been accepted.");
+            Console.Write("Press Enter to Continue.");
+            readResult = Console.ReadLine();
         }
 
         #endregion
